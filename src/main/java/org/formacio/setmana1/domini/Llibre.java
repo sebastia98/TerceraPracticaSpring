57a7e7378032262e4eb3ecedd30c4cc7605a86ae
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-05-14 13:35:00 +0200

    Feat: Mapear la entidad Llibre

--- src/main/java/org/formacio/setmana1/domini/Llibre.java
+++ src/main/java/org/formacio/setmana1/domini/Llibre.java
@@ -1,23 +1,40 @@
 package org.formacio.setmana1.domini;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * Completa amb annotacions aquesta classe per tal que sigui una entitat
  * mapejada a la taula T_LLIBRES, tal com esta definida a src/main/resources/schema.sql
  * La clau primaria es la propietat isbn. Important: aquesta clau NO se autogenera. S'assigna des de l'aplicacio
  */
-public class Llibre {
 
+@Entity
+@Table(name = "T_LLIBRES")
+public class Llibre {
+	
+	@Id
+	@Column(name = "LLI_ISBN")
 	private String isbn;
 	
+	@Column(name = "LLI_TITOL")
 	private String titol;
 	
+	@Column(name = "LLI_AUTOR")
 	private String autor;
 	
+	@Column(name = "LLI_PAGINES")
 	private Integer pagines;
 
 	// fitxeu-vos que aquesta propietat, a la base de dades, se guarda com VARCHAR2, no com INT.
 	// Aixo requeria d'una annotacio especifica
+	
+	@Column(name = "LLI_RECOMANACIO")
+	@Enumerated(EnumType.STRING)
 	private Recomanacio recomanacio;
 	
 	public String getIsbn() {
