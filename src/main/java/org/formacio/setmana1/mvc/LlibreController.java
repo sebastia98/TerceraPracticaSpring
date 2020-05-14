1ab9fc534c3d04c89837dbd870cdf8c8faa8d3e1
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-05-14 16:16:58 +0200

    Feat: Modificar controlador para responder peticion web

--- src/main/java/org/formacio/setmana1/mvc/LlibreController.java
+++ src/main/java/org/formacio/setmana1/mvc/LlibreController.java
@@ -1,8 +1,14 @@
 package org.formacio.setmana1.mvc;
 
+import org.formacio.setmana1.data.LlibreNoExisteixException;
+import org.formacio.setmana1.data.LlibreOpsBasic;
+import org.formacio.setmana1.domini.Llibre;
 import org.formacio.setmana1.domini.Recomanacio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -17,12 +23,22 @@ public class LlibreController {
 
 	// Per aqui vos fara falta una referencia a un LlibreOpsBasic
 	
+	@Autowired
+	LlibreOpsBasic ops;
+	
+	@RequestMapping(path = "/titol", method = RequestMethod.GET)
+	public String obteTitol(@RequestParam String isbn) throws LlibreNoExisteixException {
+		Llibre llibre = ops.carrega(isbn);
+		return llibre.getTitol();
+	}
+	
 	// Aquestes anotacions i firma del metode ja son correctes
 	@RequestMapping(path="/recomanacio")
 	@ResponseBody
- 	public Recomanacio obteLlibre (String isbn) {
+ 	public Recomanacio obteLlibre (String isbn) throws LlibreNoExisteixException {
 		// Feis que retorni la recomanacio per el llibre indicat 
 		// emprat LlibreOpsBasic
-		return null; 
+		Llibre llibre = ops.carrega(isbn);
+		return llibre.getRecomanacio();
 	}
 }
